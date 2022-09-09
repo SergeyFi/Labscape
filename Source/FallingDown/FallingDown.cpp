92ee8c3127abb53f52d633478e1142c1e4098b77
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2022-09-09 19:41:09 +0300

    Initial commit

---
+++ Source/FallingDown/FallingDown.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FallingDown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FallingDown, "FallingDown" );
---
+++ Source/FallingDown/FallingDownGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FallingDownGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FALLINGDOWN_API AFallingDownGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
