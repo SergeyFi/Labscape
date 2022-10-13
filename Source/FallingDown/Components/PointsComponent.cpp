d3904aee958e77352ade3682f2539f1622711cd0
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2022-10-13 08:28:47 +0300

    Feature: add points

---
+++ Source/FallingDown/Components/PointsComponent.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FallingDown/Components/PointsComponent.h"
+
+// Sets default values for this component's properties
+UPointsComponent::UPointsComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UPointsComponent::AddPoints(int32 Count)
+{
+	Points += Count;
+
+	OnPointsUpdated.Broadcast(Points);
+}
+
+int32 UPointsComponent::GetPoints()
+{
+	return Points;
+}
